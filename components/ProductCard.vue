ee86ceb03df5d5ac446486a0345c1113aae62d4c
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-11-27 15:23:58 +0700

    Import Component

---
+++ components/ProductCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <p v-if="$fetchState.pending">Fetching mountains...</p>
+    <p v-else-if="$fetchState.error">An error occurred :(</p>
+    <div
+      v-for="mountain in mountains"
+      v-else
+      :key="mountain.title"
+      class="mb-4 flex flex-col text-left md:flex"
+    >
+      <div class="md:flex-shrink-0">
+        <img
+          :src="mountain.image"
+          :alt="mountain.title"
+          class="rounded-lg md:w-56"
+        />
+      </div>
+      <div class="mt-4 md:mt-4 md:ml-6">
+        <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">
+          {{ mountain.continent }}
+        </div>
+        <NuxtLink
+          :to="mountain.slug"
+          class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline"
+        >
+          {{ mountain.title }}
+        </NuxtLink>
+        <p class="mt-2 text-gray-600">{{ mountain.description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async fetch() {
+    this.mountains = await fetch(
+      'https://api.nuxtjs.dev/mountains'
+    ).then((res) => res.json())
+  },
+  data() {
+    return {
+      mountains: [],
+    }
+  },
+}
+</script>
---
+++ pages/products.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container mx-auto mt-8">
+    <h1 class="font-bold text-xl text-center">Products</h1>
+    <button
+      class="text-sm bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded-full mb-4"
+      @click="showMountains"
+    >
+      Refresh
+    </button>
+    <LazyProductCard v-if="show" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+    }
+  },
+  methods: {
+    showMountains() {
+      this.show = true
+    },
+  },
+}
+</script>
+
+<style lang="postcss">
+img {
+  height: 120px;
+}
+</style>
